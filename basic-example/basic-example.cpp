4db24f28b599c9c3250c0259c58a2f1bf2eb9211
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2018-08-01 17:11:23 +0200

    adapted example to also test array sections but still some problems with that - fixed place where stuff needs to be mapped to host pointers - cleanup list after task has been created (maybe we need to decrement the counter here first) - implemented global task counter - implemented functions for parameter debug output

--- basic-example/basic-example.cpp
+++ basic-example/basic-example.cpp
@@ -1,15 +1,32 @@
 #ifndef USE_MPI
 #define USE_MPI 0
 #endif
+// whether or not to offload tasks
 #ifndef USE_OFFLOADING
 #define USE_OFFLOADING 0
 #endif
+// complex scenario that might use multiple OpenMP threads to create multiple target tasks
 #ifndef USE_COMPLEX
 #define USE_COMPLEX 0
 #endif
-#ifndef SECOND_TARGET_REGION
-#define SECOND_TARGET_REGION 0
+// size of the double arrays for complex scenario
+#ifndef ARR_SIZE
+#define ARR_SIZE 10
 #endif
+// number of tasks for complex scenario
+#ifndef NR_TASKS
+#define NR_TASKS 2
+#endif
+#ifndef DEV_NR
+#if USE_MPI
+#define DEV_NR 1002 // CHAMELEON_MPI
+#else
+// use standard host offloading in case no MPI is used
+#define DEV_NR 1001 // CHAMELEON_HOST
+#endif
+#endif
+
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 #include <inttypes.h>
 #include <math.h>
@@ -23,26 +40,22 @@
 #include "chameleon.h"
 #endif
 
-#define N 15
-
-#ifndef DEV_NR
-//#define DEV_NR 1001 // CHAMELEON_HOST
-#define DEV_NR 1002 // CHAMELEON_MPI
-#endif
-
 int main(int argc, char **argv)
 {
-    int n = N;
+    int n = ARR_SIZE;
     const double fPi25DT = 3.141592653589793238462643;
     double fTimeStart, fTimeEnd;
     int i;
+    int step = 0;
+
+#if USE_COMPLEX
+    double a[n];
+    int b[n];
+#else
     int scalar_A;
     double scalar_B;
     int scalar_C;
-    double a[n], b[n], c[n];
-    //double *a = (double*) malloc(sizeof(double)*n);
-    //double *b = (double*) malloc(sizeof(double)*n);
-    //double *c = (double*) malloc(sizeof(double)*n);
+#endif
 
 #if USE_MPI
     int iMyRank, iNumProcs;
@@ -66,52 +79,78 @@ int main(int argc, char **argv)
     {
 #endif
         // data initialization
-        scalar_A = 1;
-        scalar_B = 2;
-        scalar_C = 3;
+#if USE_COMPLEX
         for(i = 0; i < n; i++)
         {
-            a[i] = 1.0 / sin(i);
-            b[i] = 1.0 / cos(i);
+            a[i] = 5.0;
+            b[i] = 3;
         }
-        
+        // calculate offsets to decompose array into single tasks
+        step = ARR_SIZE / NR_TASKS;
+        if (step * NR_TASKS < ARR_SIZE) step++;
+        // for(i = 0; i < NR_TASKS; i++) {
+        //     int tmp_idx_start = i * step;
+        //     printf("Master: array_a_dbl[%d] = %f at (" DPxMOD ")\n", tmp_idx_start, a[tmp_idx_start], DPxPTR(&a[tmp_idx_start]));
+        //     printf("Master: array_b_int[%d] = %d at (" DPxMOD ")\n", tmp_idx_start, b[tmp_idx_start], DPxPTR(&b[tmp_idx_start]));
+        // }
+        for(i = 0; i < ARR_SIZE; i++) {
+            printf("Master: array_a_dbl[%02d] = %f at (" DPxMOD ")\n", i, a[i], DPxPTR(&a[i]));
+        }
+        for(i = 0; i < ARR_SIZE; i++) {
+            printf("Master: array_b_int[%02d] = %d at (" DPxMOD ")\n", i, b[i], DPxPTR(&b[i]));
+        }
+#else
+        scalar_A = 1;
+        scalar_B = 2;
+        scalar_C = 3;        
         printf("Master: scalar_A_int = %d at (" DPxMOD ")\n", scalar_A, DPxPTR(&scalar_A));
         printf("Master: scalar_B_dbl = %f at (" DPxMOD ")\n", scalar_B, DPxPTR(&scalar_B));
         printf("Master: scalar_c_int = %d at (" DPxMOD ")\n", scalar_C, DPxPTR(&scalar_C));
-
+#endif //USE_COMPLEX
+        // measure start time here
         fTimeStart = omp_get_wtime();
-#if USE_OFFLOADING
-        // first test: calculate complete block in target region
 #if USE_COMPLEX
-        #pragma omp target map(tofrom:b[0:N], a[0:N], c[0:N]) device(DEV_NR)
-#else
-        #pragma omp target map(tofrom:scalar_A) device(DEV_NR)
-#endif
-        {
-#endif
-#if USE_COMPLEX
-            int ii;
-            for(ii = 0; ii < n; ii++)
-            {
-                c[ii] = ((4.0*fPi25DT / (1.0 + a[ii]*b[ii])) + b[ii]);
+        // run multiple threads where each is creating target tasks
+        #pragma omp parallel for
+        for(i = 0; i < NR_TASKS; i++) {
+            int idx_start = i * step;
+            int cur_len = step;
+            int idx_end = i*(step+1)-1;
+            if(idx_end > ARR_SIZE-1) {
+                cur_len = ARR_SIZE-1-idx_start;
             }
-#else
+#if USE_OFFLOADING
+            #pragma omp target map(tofrom:a[idx_start:cur_len], b[idx_start:cur_len]) device(DEV_NR)
+            {
+#endif // USE_OFFLOADING
+                printf("Device: array_a_dbl[%d] = %f at (" DPxMOD ")\n", idx_start, a[idx_start], DPxPTR(&a[idx_start]));
+                printf("Device: array_b_int[%d] = %d at (" DPxMOD ")\n", idx_start, b[idx_start], DPxPTR(&b[idx_start]));
+                printf("Device: setting array_a_dbl = 13.37\n");
+                printf("Device: setting array_b_int = 42\n");
+                for(int j = 0; j < cur_len; j++) {
+                    a[idx_start+j] = 13.37;
+                    b[idx_start+j] = 42;
+                }
+#if USE_OFFLOADING
+            }
+#endif // USE_OFFLOADING
+        }
+
+#else // USE_COMPLEX
+
+#if USE_OFFLOADING
+        #pragma omp target map(tofrom:scalar_A) device(DEV_NR)
+        {
+#endif // USE_OFFLOADING
             printf("Device: Implict mapped scalar_B_dbl = %f at (" DPxMOD ")\n", scalar_B, DPxPTR(&scalar_B));
             printf("Device: Implict mapped scalar_C_int = %d at (" DPxMOD ")\n", scalar_C, DPxPTR(&scalar_C));
             printf("Device: scalar_A_int = %d at (" DPxMOD ")\n", scalar_A, DPxPTR(&scalar_A));
             printf("Device: Setting scalar_A_int = 42\n");
             scalar_A = 42; 
-#endif
 #if USE_OFFLOADING
         }
-#endif
-#if USE_OFFLOADING && SECOND_TARGET_REGION
-        #pragma omp target map(tofrom:scalar_A) device(DEV_NR)
-        {
-            printf("Device: scalar_A_int = %d at (" DPxMOD ")\n", scalar_A, DPxPTR(&scalar_A));
-            printf("Device: scalar_B_dbl = %f at (" DPxMOD ")\n", scalar_B, DPxPTR(&scalar_B));
-        }
-#endif
+#endif // USE_OFFLOADING
+#endif // USE_COMPLEX
 #if USE_MPI
     }
     
@@ -123,21 +162,27 @@ int main(int argc, char **argv)
     {
 #endif
         fTimeEnd = omp_get_wtime() - fTimeStart;
+        printf("Elapsed computation time: %.3f\n", fTimeEnd);
+        printf("Results:\n");
+#if USE_COMPLEX
+        // for(i = 0; i < NR_TASKS; i++) {
+        //     int tmp_idx_start = i*step;
+        //     printf("Master: array_a_dbl[%d] = %f at (" DPxMOD ")\n", tmp_idx_start, a[tmp_idx_start], DPxPTR(&a[tmp_idx_start]));
+        //     printf("Master: array_b_int[%d] = %d at (" DPxMOD ")\n", tmp_idx_start, b[tmp_idx_start], DPxPTR(&b[tmp_idx_start]));
+        // }
+        for(i = 0; i < ARR_SIZE; i++) {
+            printf("Master: array_a_dbl[%02d] = %f at (" DPxMOD ")\n", i, a[i], DPxPTR(&a[i]));
+        }
+        for(i = 0; i < ARR_SIZE; i++) {
+            printf("Master: array_b_int[%02d] = %d at (" DPxMOD ")\n", i, b[i], DPxPTR(&b[i]));
+        }
+#else
         printf("Master: scalar_A_int = %d at (" DPxMOD ")\n", scalar_A, DPxPTR(&scalar_A));
         printf("Master: scalar_B_dbl = %f at (" DPxMOD ")\n", scalar_B, DPxPTR(&scalar_B));
-        printf("Elapsed computation time: %.3f\n", fTimeEnd);        
-#if USE_COMPLEX
-        printf("Results:\n");
-        for(i = 0; i < 5; i++)
-        {
-            printf("c[%d] = %f\n", i, c[i]);
-        }
-        printf("...\n");
 #endif
 #if USE_MPI
-    }
-    
-    //chameleon_finalize();
+    }    
+    chameleon_finalize();
     MPI_Finalize();
 #endif
     return 0;
