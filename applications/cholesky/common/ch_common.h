07865ed84650d15d4132fd47eaa5521f19e5ec44
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2020-03-31 01:13:45 +0200

    fixed indentation and implemented debug prints with task stack depth information

--- applications/cholesky/common/ch_common.h
+++ applications/cholesky/common/ch_common.h
@@ -22,6 +22,16 @@
 #include "VT.h"
 #endif
 
+#ifdef MAIN
+int np;
+int mype;
+int num_threads;
+#else
+extern int np;
+extern int mype;
+extern int num_threads;
+#endif
+
 #if defined(USE_TIMING)
 void helper_start_timing(int tt);
 void helper_end_timing(int tt, double elapsed);
@@ -32,13 +42,6 @@ void helper_end_timing(int tt, double elapsed);
 
 #if defined(CHAMELEON) || defined(CHAMELEON_TARGET)
 #include "chameleon.h"
-#ifndef my_print
-#define my_print(...) chameleon_print(0, "Cholesky", mype, __VA_ARGS__);
-#endif
-#else
-#ifndef my_print
-#define my_print(...) fprintf(stderr, __VA_ARGS__);
-#endif
 #endif
 
 #ifndef DPxMOD
@@ -49,8 +52,37 @@ void helper_end_timing(int tt, double elapsed);
 #define DPxPTR(ptr) ((int)(2*sizeof(uintptr_t))), ((uintptr_t) (ptr))
 #endif
 
-#ifndef PRINT_DEBUG
-#define PRINT_DEBUG 0
+#ifdef DEBUG
+int* __task_depth_counter;
+#define TASK_DEPTH_OFFSET 32
+#define TASK_DEPTH_INIT do { \
+    __task_depth_counter = (int*) malloc((omp_get_max_threads()+1)*TASK_DEPTH_OFFSET*sizeof(int)); \
+    for (int i = 0; i < omp_get_max_threads(); i++) { \
+        __task_depth_counter[i*TASK_DEPTH_OFFSET] = 0; \
+    } \
+} while(0)
+#define TASK_DEPTH_FINALIZE \
+    if(__task_depth_counter) \
+        free(__task_depth_counter);
+
+#define TASK_DEPTH_INCR ++__task_depth_counter[omp_get_thread_num()*TASK_DEPTH_OFFSET]
+#define TASK_DEPTH_DECR --__task_depth_counter[omp_get_thread_num()*TASK_DEPTH_OFFSET]
+#define TASK_DEPTH_GET __task_depth_counter[omp_get_thread_num()*TASK_DEPTH_OFFSET]
+#define DEBUG_PRINT(STR, ...) do { \
+    char* tmp_str = malloc(sizeof(char)*512); \
+    tmp_str[0] = '\0'; \
+    strcat(tmp_str,"R#%02d T#%02d (OS_TID:%06ld) Task-Depth:%02d : --> "); \
+    strcat(tmp_str,STR); \
+    fprintf(stderr, tmp_str, mype, omp_get_thread_num(), syscall(SYS_gettid), TASK_DEPTH_GET, __VA_ARGS__); \
+    free(tmp_str); \
+} while(0)
+#else
+#define TASK_DEPTH_INIT
+#define TASK_DEPTH_FINALIZE
+#define TASK_DEPTH_INCR
+#define TASK_DEPTH_DECR
+#define TASK_DEPTH_GET 0
+#define DEBUG_PRINT(STR, ...)
 #endif
 
 #ifdef _USE_HBW
@@ -81,44 +113,33 @@ void wait(MPI_Request *comm_req);
 
 inline static void waitall(MPI_Request *comm_req, int n)
 {
-// #ifdef TRACE
-//     static int event_waitall = -1;
-//     char* event_name = "waitall";
-//     if(event_waitall == -1) {
-//         int ierr;
-//         ierr = VT_funcdef(event_name, VT_NOCLASS, &event_waitall);
-//     }
-//     VT_begin(event_waitall);
-// #endif
-#ifdef DISABLE_TASKYIELD
-  MPI_Waitall(n, comm_req, MPI_STATUSES_IGNORE);
-#else
-  while (1) {
-    int flag = 0;
-    MPI_Testall(n, comm_req, &flag, MPI_STATUSES_IGNORE);
-    if (flag) break;
-    (void)flag; // <-- make the Cray compiler happy
-#if defined(CHAMELEON) || defined(CHAMELEON_TARGET)
-    int32_t res = chameleon_taskyield();
-#else
-    #pragma omp taskyield
-#endif
-  }
-#endif
-// #ifdef TRACE
-//     VT_end(event_waitall);
-// #endif
+    #ifdef TRACE
+    static int event_waitall = -1;
+    if(event_waitall == -1) {
+        char* event_name = "waitall";
+        int ierr;
+        ierr = VT_funcdef(event_name, VT_NOCLASS, &event_waitall);
+    }
+    VT_begin(event_waitall);
+    #endif
+    #ifdef DISABLE_TASKYIELD
+    MPI_Waitall(n, comm_req, MPI_STATUSES_IGNORE);
+    #else
+    while (1) {
+        int flag = 0;
+        MPI_Testall(n, comm_req, &flag, MPI_STATUSES_IGNORE);
+        if (flag) break;
+        (void)flag; // <-- make the Cray compiler happy
+        #if defined(CHAMELEON) || defined(CHAMELEON_TARGET)
+            int32_t res = chameleon_taskyield();
+        #else
+            #pragma omp taskyield
+        #endif
+    }
+    #endif
+    #ifdef TRACE
+    VT_end(event_waitall);
+    #endif
 }
 void reset_send_flags(char *send_flags);
-
-#ifdef MAIN
-int np;
-int mype;
-int num_threads;
-#else
-extern int np;
-extern int mype;
-extern int num_threads;
-#endif
-
 #endif
